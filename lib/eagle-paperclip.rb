e1522ecbb6bd7d84808d6b189ed01b89c6eeec36
gropher <grophen@gmail.com>
2013-07-01 15:37:56 +0400

    URL: account_code interpolation added

--- lib/eagle-paperclip.rb
+++ lib/eagle-paperclip.rb
@@ -1,3 +1,7 @@
 require "eagle-paperclip/version"
 
 require "paperclip/storage/eagle"
+
+Paperclip.interpolates('account_code') do |attachment, style|
+  attachment.instance.account.code.parameterize
+end
